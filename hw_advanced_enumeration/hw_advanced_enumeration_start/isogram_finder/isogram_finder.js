4c7335fb816a5431f1163b120574fd4518eff9e5
Vicky Jackson <vicky@digitalstuff.net>
2018-09-05 22:54:04 +0100

    Finished the isogram challenge

--- hw_advanced_enumeration/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,22 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  let result = [];
+  const baseArray = this.word.toLowerCase().split('');
+  const newArray = baseArray.slice()
+  newArray.forEach(letter => {
+    if (!result.includes(letter)){
+      result.push(letter);
+    }
+  })
+  console.log(newArray);
+  console.log(result);
+  if (newArray.sort().toString() !== result.sort().toString()){
+    return false;
+  }
+  return true;
 }
 
 module.exports = IsogramFinder;
